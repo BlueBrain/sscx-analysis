0721bcebe548f9bf70734fd7bffd5b7571c4f396
Christoph Pokorny <christoph.pokorny@epfl.ch>
2021-06-30 17:44:30 +0200

    Improvements to analysis launcher

--- analysis_launcher/README.md
+++ analysis_launcher/README.md
@@ -6,4 +6,4 @@ Description TBA...
 * [bbp-workflow CLI](https://bbpteam.epfl.ch/project/spaces/pages/viewpage.action?spaceKey=BBPNSE&title=Workflow)
 
 ## How to run:
-* bbp-workflow launch --follow --config workflows/campaign_analysis_launcher.cfg sim_campaign_analyses CampaignAnalysisLauncher
+* bbp-workflow bbp-workflow launch --follow campaign_analysis_launcher CampaignAnalysisLauncher
--- analysis_launcher/workflows/campaign_analysis_launcher.py
+++ analysis_launcher/workflows/campaign_analysis_launcher.py
@@ -3,34 +3,39 @@
 # Created: 28/06/2021
 
 import os
+import json
+import subprocess
 import numpy as np
-from luigi import ListParameter, DictParameter, WrapperTask
+from luigi import Task, Parameter, ListParameter, DictParameter
 from bbp_workflow.simulation import LookupSimulationCampaign
 from bbp_workflow.utils import xr_from_dict
 from bbp_workflow.luigi import RunAnywayTarget
 from bbp_workflow.task import SbatchTask
 
-""" Campaign analysis launcher (wrapper task), launching separate analysis tasks as specified """
-class CampaignAnalysisLauncher(WrapperTask):
+""" Campaign analysis launcher, preparing config files and launching separate analysis tasks as specified """
+class CampaignAnalysisLauncher(Task):
     
-    analyses = ListParameter(default=[])
+    list_of_analyses = ListParameter(default=[])
     
     def requires(self):
+        return LookupSimulationCampaign()
+    
+    def run(self):
         
         # Load simulation campaign config from Nexus URL
-        sim_campaign_cfg = LookupSimulationCampaign().output().entity
+        sim_campaign_cfg = sim_campaign_cfg = self.input().entity
         config = xr_from_dict(sim_campaign_cfg.configuration.as_dict())  # Get sim campaign config as Xarray
         root_path = os.path.join(config.attrs['path_prefix'], config.name) # Root path of simulation campaign
         sim_paths = config.to_series() # Single simulation paths as Pandas series with multi-index
         assert os.path.commonpath(sim_paths.tolist()) == root_path, 'ERROR: Root path mismatch!'
         
-        print(f'\nINFO: Loaded simulation campaign "{sim_campaign_cfg.name}" from {sim_campaign_cfg.get_url()} with coordinates {list(sim_paths.index.names)}\n')
+        print(f'\nINFO: Loaded simulation campaign "{sim_campaign_cfg.name}" from {sim_campaign_cfg.get_url()} with coordinates {list(sim_paths.index.names)}')
         
         # Check if simulation results exist
         valid_sims = [os.path.exists(os.path.join(p, 'out.dat')) for p in sim_paths]
         sim_paths = sim_paths[valid_sims]
         
-        print(f'\nINFO: Found {np.sum(valid_sims)} of {len(valid_sims)} completed simulations\n')
+        print(f'INFO: Found {np.sum(valid_sims)} of {len(valid_sims)} completed simulations to analyze')
         
         # Create simulation paths to BlueConfigs
         sims = sim_paths.apply(lambda p: os.path.join(p, 'BlueConfig'))
@@ -40,47 +45,71 @@ class CampaignAnalysisLauncher(WrapperTask):
         launch_path = os.path.join(root_path, 'analyses')
         if not os.path.exists(launch_path):
             os.makedirs(launch_path)
-        
-        sims.to_pickle(os.path.join(launch_path, 'simulations.pkl'))
+        sim_file = 'simulations.pkl'
+        sims.to_pickle(os.path.join(launch_path, sim_file))
         
         # Prepare & launch analyses, as specified in launch config
-        num_analyses = len(self.analyses)
-        print(f'INFO: Launching {num_analyses} analyses...')
+        num_analyses = len(self.list_of_analyses)
+        print(f'INFO: {num_analyses} campaign analyses to launch: {[anlys["name"] for anlys in self.list_of_analyses]}\n')
         
-        # TODO: Write config files
-        #       Clone scripts
-        #       Launch scripts
+        analysis_tasks = []
+        for anlys in self.list_of_analyses:
+            anlys_name = anlys['name']
+            anlys_script = anlys['script']
+            anlys_params = dict(anlys['parameters'])
+            anlys_res = anlys['resources']
+            
+            # Create script folder
+            script_path = os.path.join(launch_path, 'scripts', anlys_name)
+            if not os.path.exists(script_path):
+                os.makedirs(script_path)
+            
+            # Write parameters
+            anlys_params['out_root'] = script_path
+            param_file = 'parameters.json'
+            with open(os.path.join(script_path, param_file), 'w') as f:
+                json.dump(anlys_params, f, indent=2)
+            
+            # Download script from GIT repository to script_path
+            script_name = os.path.split(anlys_script)[-1]
+            script_file = os.path.join(script_path, script_name)
+            # TODO...
+#             if os.path.isfile(script_file):
+#                 os.remove(script_file) # Remove if already exists
+#             proc = subprocess.Popen(f'wget -P {script_path} {anlys_script}', shell=True, stdout=subprocess.PIPE)
+#             print(proc.communicate()[0].decode())
+            
+            # Prepare tasks
+            cmd = f'python -u {script_name}'
+            args = f'{os.path.join(os.path.relpath(launch_path, script_path), sim_file)} {param_file}'
+            module_archive = anlys.get('module_archive', 'unstable')
+            modules = anlys.get('modules', 'python') # Loading latest Python module by default
+            anlys_res = {k: str(v) for k, v in anlys_res.items()} # Convert values to str, to avoid warning from parameter parser when directly passing whole "resources" dict
+            analysis_tasks.append(CampaignAnalysis(name=anlys_name, chdir=script_path, command=cmd, args=args, module_archive=module_archive, modules=modules, **anlys_res))
         
-        return [] #self.analyses()
-    
-#     def analyses(self):
-        
-#         num_analyses = len(self.analyses)
-#         print(f'RUNNING {num_analyses} analyses...')
-        
-#         return [CampaignAnalysis(analysis_params={'name': f'test{n}'}) for n in range(num_analyses)]
-
-
-""" Campaign analysis task, running an analysis as SLURM job """
-class CampaignAnalysis(SbatchTask):
-    
-    analysis_params = DictParameter(default={})
-    command = ''
-    
-    def run(self):
-        print(f'RUNNING Analysis with params: {self.analysis_params}')
-        
-        self.command = 'python -u payload.py'
-        self.args = ''
-        self.chdir = '/gpfs/bbp.cscs.ch/project/proj83/home/pokorny/bbp-workflow/workflows/testing/workflows'
-        self.job_name = self.analysis_params.get('name', 'unknown') + '_analysis'
-        self.module_archive = 'unstable'
-        self.modules = 'python' # Loading latest Python module
-        
-        super().run()
+        yield analysis_tasks # Launch tasks
         
         self.output().done()
     
     def output(self):
         return RunAnywayTarget(self)
 
+
+""" Campaign analysis task, running an analysis as SLURM job """
+class CampaignAnalysis(SbatchTask):
+    
+    name = Parameter()
+    
+    def run(self):
+        print(f'\nINFO: Running campaign analysis task "{self.name}"\n')
+        print(f'Command: {self.command} {self.args}') # [TESTING]
+        print(f'Modules: {self.module_archive} {self.modules}') # [TESTING]
+        
+        self.job_name = 'CampaignAnalysis[' + self.name + ']'
+        
+        #super().run()
+        
+        self.output().done()
+    
+    def output(self):
+        return RunAnywayTarget(self)
