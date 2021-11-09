e95eca253e36dfa9cf630694cd40fddd67925536
Ecker András <andras.ecker@epfl.ch>
2021-11-09 11:33:43 +0100

    refactoring plot_raster

--- plasticity/plot_raster.py
+++ plasticity/plot_raster.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 """
 Plot SSCx hex_O1 rasters
-author: András Ecker, last update: 10.2021
+author: András Ecker, last update: 11.2021
 """
 
 import os
 import time
 import pickle
 import numpy as np
-import pandas as pd
 from bluepy import Simulation
+import utils
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -28,35 +27,7 @@ legend_handles = [mlines.Line2D([], [], color=RED, marker="s", linestyle='None',
                   mlines.Line2D([], [], color=GREEN, marker="s", linestyle='None', markersize=10, label="Sst"),
                   mlines.Line2D([], [], color=ORANGE, marker="s", linestyle='None', markersize=10, label="5HT3aR")]
 PROJ_COLORS = {"VPM": "#4a4657", "POm": "#442e8a"}
-SIMS_DIR = "/gpfs/bbp.cscs.ch/project/proj96/scratch/home/ecker/simulations"
 FIGS_DIR = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/figures/sscx-analysis"
-
-
-def _ensure_dir(dir_path):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-
-def _idx2str(level_names, idx):
-    """Helper function to convert MultiIndex DataFrame index to string"""
-    str_ = ""
-    for i, level_name in enumerate(level_names):
-        value = ("%.2f" % idx[i]).replace('.', 'p') if isinstance(idx[i], float) else "%s" % idx[i]
-        str_ += "%s%s_" % (level_name, value)
-    return str_
-
-
-def get_spikes(sim, t_start, t_end):
-    """Extracts spikes with bluepy"""
-    spikes = sim.spikes.get(t_start=t_start, t_end=t_end)
-    return spikes.index, spikes.values
-
-
-def _calc_rate(spike_times, N, t_start, t_end, bin_size):
-    """Calculates populational firing rate"""
-    t_bins = np.arange(t_start, t_end+bin_size, bin_size)
-    rate, _ = np.histogram(spike_times, t_bins)
-    return rate / (N*1e-3*bin_size)  # *1e-3 ms to s conversion
 
 
 def setup_raster(spike_times, spiking_gids, ys, colors, groups, types, type_colors, t_start, t_end):
@@ -78,32 +49,24 @@ def setup_raster(spike_times, spiking_gids, ys, colors, groups, types, type_colo
 
     rates_dict = {}
     for type_, type_color in zip(types, type_colors):
-        rates_dict[type_] = _calc_rate(spike_times[cols == type_color], Ns[type_], t_start, t_end, bin_size=10)
+        rates_dict[type_] = utils.calc_rate(spike_times[cols == type_color], Ns[type_], t_start, t_end)
 
     return spiking_ys, cols, rates_dict
 
 
 def get_tc_rates(sim, t_start, t_end):
-    """Read VPM and POm spikes from spike replay file (has loads of hard coded parts...)"""
-    sim_dir = sim.config.Run_Default.CurrentDir
-    proj_dir = os.path.join(os.path.split(sim_dir)[0], "projections")
-    if os.path.isdir(proj_dir):
-        vpm_gids = np.loadtxt(os.path.join(proj_dir, "Thalamocortical_input_VPM_hex_O1.txt")).astype(int)[:, 0]
-        pom_gids = np.loadtxt(os.path.join(proj_dir, "Thalamocortical_input_POM_hex_O1.txt")).astype(int)[:, 0]
-        tmp = np.loadtxt(os.path.join(sim_dir, "input.dat"), skiprows=1)
-        spike_times, spiking_gids = tmp[:, 0], tmp[:, 1].astype(int)
-        idx = np.where((t_start < spike_times) & (spike_times < t_end))[0]
-        spike_times, spiking_gids = spike_times[idx], spiking_gids[idx]
-        proj_rate_dict = {}
-        for proj_name, proj_gids in zip(["VPM", "POm"], [vpm_gids, pom_gids]):
+    """Read VPM and POm spikes from spike replay file"""
+    spike_times, spiking_gids = utils.get_tc_spikes(sim, t_start, t_end)
+    vpm_gids, pom_gids = utils.load_tc_gids(os.path.split(sim.config.Run_Default.CurrentDir)[0])
+    proj_rate_dict = {}
+    for proj_name, proj_gids in zip(["VPM", "POm"], [vpm_gids, pom_gids]):
+        if proj_gids is not None:
             mask = np.isin(spiking_gids, proj_gids)
             if mask.sum() > 0:
                 proj_spike_times, proj_spiking_gids = spike_times[mask], spiking_gids[mask]
-                proj_rate_dict[proj_name] = _calc_rate(proj_spike_times, len(np.unique(proj_spiking_gids)),
-                                                       t_start, t_end, bin_size=10)
-        return proj_rate_dict
-    else:
-        return None
+                proj_rate_dict[proj_name] = utils.calc_rate(proj_spike_times, len(np.unique(proj_spiking_gids)),
+                                                            t_start, t_end)
+    return proj_rate_dict if len(proj_rate_dict) else None
 
 
 def plot_raster(spike_times, spiking_gids, proj_rate_dict, asthetics, t_start, t_end, fig_name):
@@ -122,7 +85,7 @@ def plot_raster(spike_times, spiking_gids, proj_rate_dict, asthetics, t_start, t
         gs = gridspec.GridSpec(4, 1, height_ratios=[14, 1, 1, 1])
     ax = fig.add_subplot(gs[0])
     ax.set_facecolor((0.95, 0.95, 0.95))
-    ax.scatter(spike_times, spiking_ys, c=cols, alpha=0.9, marker='.', s=2.5, edgecolor="none")
+    ax.scatter(spike_times, spiking_ys, c=cols, alpha=0.9, marker='.', s=2., edgecolor="none")
     ax.set_xlim([t_start, t_end])
     ax.set_ylim([np.max(asthetics["yticks"]), np.min(asthetics["yticks"])])
     ax.set_yticks(asthetics["yticks"])
@@ -166,24 +129,60 @@ def plot_raster(spike_times, spiking_gids, proj_rate_dict, asthetics, t_start, t
     plt.close(fig)
 
 
+def plot_patterns(pattern_gids, all_gids, pos, patterns_dir):
+    """Plots projection patterns in flatmap space"""
+    utils.ensure_dir(patterns_dir)
+    cmap = plt.cm.get_cmap("tab10", 10)
+    fig = plt.figure(figsize=(10, 9))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(pos[:, 0], pos[:, 1], color="gray", marker='.', s=50, alpha=0.5)
+    ax.set_xlabel("x (flat space)")
+    ax.set_ylabel("y (flat space)")
+    sns.despine()
+    for i, (name, gids) in enumerate(pattern_gids.items()):
+        pattern_pos = pos[np.isin(all_gids, gids), :]
+        if name in ["A", "B", "C", "D"]:
+            ax.scatter(pattern_pos[:, 0], pattern_pos[:, 1], color=cmap(i), marker='.', s=100, label=name)
+        fig2 = plt.figure(figsize=(10, 9))
+        ax2 = fig2.add_subplot(1, 1, 1)
+        ax2.scatter(pos[:, 0], pos[:, 1], color="gray", marker='.', s=50, alpha=0.5)
+        ax2.scatter(pattern_pos[:, 0], pattern_pos[:, 1], color=cmap(i), marker='.', s=100)
+        ax2.set_xlabel("x (flat space)")
+        ax2.set_ylabel("y (flat space)")
+        sns.despine()
+        fig_name = os.path.join(patterns_dir, "pattern_%s.png" % name)
+        fig2.savefig(fig_name, bbox_inches="tight", dpi=100)
+        plt.close(fig2)
+    ax.legend(frameon=False)
+    fig_name = os.path.join(patterns_dir, "patterns_ABCD.png")
+    fig.savefig(fig_name, bbox_inches="tight", dpi=100)
+    plt.close(fig)
+
+
 if __name__ == "__main__":
 
-    project_name = "63613995-0cac-44e5-b952-1e651141c69f"
-    t_start = 0
-    pklf_name = os.path.join(SIMS_DIR, project_name, "analyses", "simulations.pkl")
-    sim_paths = pd.read_pickle(pklf_name)
+    project_name = "c52688f7-eda4-4df0-af00-85a20977c9ab"
+    t_start = 1000
+    plt_patterns = False
+
+    sim_paths = utils.load_sim_path(project_name)
     level_names = sim_paths.index.names
-    _ensure_dir(os.path.join(FIGS_DIR, project_name))
+    utils.ensure_dir(os.path.join(FIGS_DIR, project_name))
     with open("raster_asth.pkl", "rb") as f:
         raster_asthetics = pickle.load(f)
 
     for idx, sim_path in sim_paths.iteritems():
         sim = Simulation(sim_path)
         t_end = sim.t_end
-        spike_times, spiking_gids = get_spikes(sim, t_start, t_end)
+        spike_times, spiking_gids = utils.get_spikes(sim, t_start, t_end)
         proj_rate_dict = get_tc_rates(sim, t_start, t_end)
-        fig_name = os.path.join(FIGS_DIR, project_name, "%sraster.png" % _idx2str(level_names, idx))
+        fig_name = os.path.join(FIGS_DIR, project_name, "%sraster.png" % utils.midx2str(idx, level_names))
         plot_raster(spike_times, spiking_gids, proj_rate_dict, raster_asthetics, t_start, t_end, fig_name)
+
+    if plt_patterns:
+        plot_patterns(*utils.load_patterns(project_name), os.path.join(FIGS_DIR, project_name, "patterns"))
+
+
 
 
 
