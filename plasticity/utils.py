8127c1271f7d9d97e7e0bd8a59fad5c4a63de391
Ecker András <andras.ecker@epfl.ch>
2022-01-25 13:55:29 +0100

    cluster loader updated to match latest

--- plasticity/utils.py
+++ plasticity/utils.py
@@ -121,15 +121,21 @@ def load_patterns(project_name, seed=None):
         raise RuntimeError("Couldn't find saved *pattern_gids*.pkl in %s/input_spikes" % project_name)
 
 
-def load_synapse_clusters(seed, sim_path):
+def load_synapse_clusters(seed, sim_path, late_assembly=False):
     """Loads synapse clusters for given `seed` saved by `assemblyfire`"""
     base_dir = os.path.join(os.path.split(os.path.split(sim_path)[0])[0], "analyses", "seed%i_syn_clusters" % seed)
     syn_clusters, gids = {}, np.array([])
     for f_name in os.listdir(base_dir):
-        if f_name[-4:] == ".pkl":
-            df = pd.read_pickle(os.path.join(base_dir, f_name))
-            syn_clusters[int(f_name.split("assembly")[1].split(".pkl")[0])] = df
-            gids = np.concatenate((gids, df["post_gid"].unique()))
+        if late_assembly:
+            if f_name[:5] == "late_" and f_name[-4:] == ".pkl":
+                df = pd.read_pickle(os.path.join(base_dir, f_name))
+                syn_clusters[int(f_name.split("assembly")[1].split(".pkl")[0])] = df
+                gids = np.concatenate((gids, df["post_gid"].unique()))
+        else:
+            if f_name[:5] != "late_" and f_name[-4:] == ".pkl":
+                df = pd.read_pickle(os.path.join(base_dir, f_name))
+                syn_clusters[int(f_name.split("assembly")[1].split(".pkl")[0])] = df
+                gids = np.concatenate((gids, df["post_gid"].unique()))
     return syn_clusters, gids
 
 
