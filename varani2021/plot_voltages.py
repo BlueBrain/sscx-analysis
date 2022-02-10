f9384c16344560d173a8453f96faed0e52f70987
Ecker András <andras.ecker@epfl.ch>
2022-02-10 17:13:22 +0100

    plots separated and extra epsp analysis added

--- varani2021/plot_voltages.py
+++ varani2021/plot_voltages.py
@@ -1,21 +1,15 @@
-# -*- coding: utf-8 -*-
 """
-Plots voltage collage (of L2/3 PCs to replicate the "analysis" of Varani et al. 2021)
-author: András Ecker, last update: 11.2021
+Plots voltage collage and subthreshold EPSP amplitudes of L2/3 PCs (to replicate the "analysis" of Varani et al. 2021)
+author: András Ecker, last update: 02.2022
 """
 
 import os
 import numpy as np
+from efel import getFeatureValues
 from bluepy import Simulation
 from bluepy.enums import Cell
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import seaborn as sns
+from plots import plot_voltages, plot_all_voltages, plot_epsps_amplitudes
 
-sns.set(style="ticks", context="notebook")
-RED = "#e32b14"
 SPIKE_TH = -30  # -30 mV is NEURON's built in spike threshold
 SIMS_DIR = "/gpfs/bbp.cscs.ch/project/proj96/scratch/home/ecker/simulations"
 FIGS_DIR = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/figures/sscx-analysis"
@@ -32,32 +26,7 @@ def get_spikes(sim, gids, t_start, t_end):
         spikes = sim.spikes.get(gids=gids, t_start=t_start, t_end=t_end)
     else:
         spikes = sim.spikes.get(t_start=t_start, t_end=t_end)
-    return spikes.index.to_numpy(), spikes.values
-
-
-def get_voltages(sim, gids, t_start, t_end):
-    """Extracts reported soma voltages with bluepy"""
-    report = sim.report("soma")
-    if gids is not None:
-        # data = report.get(gids=gids, t_start=t_start, t_end=t_end)
-        data = report.get(t_start=t_start, t_end=t_end)
-        data = data[gids]
-    else:
-        data = report.get(t_start=t_start, t_end=t_end)
-    return data.index.to_numpy(), data.values
-
-
-def _clean_voltages(voltages, spiking):
-    """Removes non-spiking voltage traces if spiking is True or the spiking ones if it's False
-    (currently we detect spikes at the AIS and report the voltage of the soma...)
-    and reorders the remaining ones based on the mean value for better plotting"""
-    max_v = np.max(voltages, axis=0)
-    if spiking:
-        voltages = voltages[:, max_v > SPIKE_TH]
-    else:
-        voltages = voltages[:, max_v < SPIKE_TH]
-    idx = np.argsort(np.mean(voltages, axis=0))
-    return voltages[:, idx]
+    return spikes.index.to_numpy(), spikes.to_numpy()
 
 
 def calc_rate(spike_times, N, t_start, t_end, bin_size):
@@ -67,47 +36,54 @@ def calc_rate(spike_times, N, t_start, t_end, bin_size):
     return rate / (N*1e-3*bin_size)  # *1e-3 ms to s conversion
 
 
-def plot_voltages(v_spiking, v_subtreshold, rate, t_start, t_end, fig_name):
-    """Plots voltage collages of spiking and non-spiking cells"""
-    v_subtreshold_mean = np.mean(v_subtreshold, axis=1)
-    t_v = np.linspace(t_start, t_end, len(v_subtreshold_mean))
-    t_rate = np.linspace(t_start, t_end, len(rate))
-    xticks = np.linspace(t_start, t_end, 6).astype(int)
+def _clean_voltages(gids, voltages, spiking):
+    """Removes non-spiking voltage traces if spiking is True or the spiking ones if it's False
+    (currently we detect spikes at the AIS and report the voltage of the soma...)
+    and reorders the remaining ones based on the mean value for better plotting"""
+    max_v = np.max(voltages, axis=0)
+    idx = np.where(max_v >= SPIKE_TH)[0] if spiking else np.where(max_v < SPIKE_TH)[0]
+    voltages = voltages[:, idx]
+    gids = gids[idx]
+    sort_idx = np.argsort(np.mean(voltages, axis=0))
+    return gids[sort_idx], voltages[:, sort_idx]
 
-    fig = plt.figure(figsize=(20, 9))
-    gs = gridspec.GridSpec(4, 2, height_ratios=[1, 4, 15, 1], width_ratios=[69, 1])
-    ax = fig.add_subplot(gs[0, 0])
-    ax.plot(t_rate, rate, color=RED)
-    ax.fill_between(t_rate, np.zeros_like(t_rate), rate, color=RED, alpha=0.1)
-    ax.set_xticks(xticks)
-    ax.set_xlim(t_start, t_end)
-    ax.set_ylabel("Rate (Hz)")
-    sns.despine(ax=ax)
-    ax2 = fig.add_subplot(gs[1, 0])
-    i2 = ax2.imshow(v_spiking.T, cmap="inferno", aspect="auto", origin="lower")
-    plt.colorbar(i2, cax=fig.add_subplot(gs[1, 1]))
-    ax2.set_ylabel("Spiking gids")
-    ax2.set_xticks(np.linspace(0, v_spiking.shape[0], 6).astype(int))
-    ax2.set_xticklabels(xticks)
-    ax3 = fig.add_subplot(gs[2, 0])
-    i3 = ax3.imshow(v_subtreshold.T, cmap="inferno", aspect="auto", origin="lower")
-    cbar = plt.colorbar(i3, cax=fig.add_subplot(gs[2, 1]))
-    cbar.set_label("Voltage (mV)")
-    ax3.set_ylabel("Non-spiking gids")
-    ax3.set_xticks(np.linspace(0, v_subtreshold.shape[0], 6).astype(int))
-    ax3.set_xticklabels(xticks)
-    ax4 = fig.add_subplot(gs[3, 0])
-    ax4.plot(t_v, v_subtreshold_mean, "k-")
-    ax4.set_xticks(xticks)
-    ax4.set_xlim(t_start, t_end)
-    ax4.set_xlabel("Time (ms)")
-    ax4.set_ylabel("V (mV)")
-    ax4.set_yticks([np.floor(np.mean(v_subtreshold_mean))])
-    sns.despine(ax=ax4)
-    fig.align_ylabels()
-    gs.tight_layout(fig, h_pad=0.2, w_pad=0.2)
-    fig.savefig(fig_name, bbox_inches="tight", dpi=100)
-    plt.close(fig)
+
+def get_voltages(sim, gids, t_start, t_end, spiking):
+    """Extracts reported soma voltages with bluepy and cleanes them (see `_clean_voltages()` above)"""
+    report = sim.report("soma")
+    if gids is not None:
+        # data = report.get(gids=gids, t_start=t_start, t_end=t_end)
+        data = report.get(t_start=t_start, t_end=t_end)
+        data = data[gids]
+    else:
+        data = report.get(t_start=t_start, t_end=t_end)
+    t = data.index.to_numpy()
+    voltages = data.to_numpy()
+    gids, voltages =  _clean_voltages(gids, voltages, spiking)
+    return gids, t, voltages.T
+
+
+def efel_traces(t, voltages, t_window):
+    """Get traces in the format expected by efel.getFeatureValues."""
+    t_window[0] = t[0] if t_window[0] < t[0] else t_window[0]
+    t_window[1] = t[-1] if t_window[1] > t[-1] else t_window[1]
+    return [{"T": t, "V": v, "stim_start": [t_window[0]], "stim_end": [t_window[1]]} for v in voltages]
+
+
+def get_epsp_amplitudes(t, voltages, t_window):
+    """Get EPSP amplitudes in `vs` in `t_window` using `efel`"""
+    traces = efel_traces(t, voltages, t_window)
+    traces_results = getFeatureValues(traces, ["maximum_voltage", "voltage_base"])
+    epsps = [np.abs(trace_result["maximum_voltage"][0] - trace_result["voltage_base"][0])
+             for trace_result in traces_results]
+    return np.asarray(epsps)
+
+
+def get_n_highest_epsp_voltages(gids, voltages, epsps, n=10):
+    """Returns the gids and voltage traces corresponding to the `n` highest EPSP amplitudes"""
+    sort_idx = np.argsort(epsps)[::-1]
+    gids, voltages = gids[sort_idx], voltages[sort_idx, :]
+    return gids[:n], voltages[:n, :]
 
 
 def main():
@@ -115,18 +91,26 @@ def main():
     t_start, t_end = 2000, 2050
     _ensure_dir(os.path.join(FIGS_DIR, project_name))
 
+    # get L23 PC spikes and rate
     sim = Simulation(os.path.join(SIMS_DIR, project_name, "BlueConfig"))
     l23pc_gids = sim.circuit.cells.ids({"$target": sim.target, Cell.LAYER: [2, 3], Cell.SYNAPSE_CLASS: "EXC"})
     spike_times, spiking_gids = get_spikes(sim, l23pc_gids, t_start, t_end)
     l23pc_rate = calc_rate(spike_times, len(l23pc_gids), t_start, t_end, bin_size=1)
+    # split L23 PCs and get soma voltages for both groups
     spiking_l23pc_gids = np.unique(spiking_gids)
     non_spiking_l23pc_gids = l23pc_gids[np.isin(l23pc_gids, spiking_l23pc_gids, assume_unique=True, invert=True)]
-    _, v_spiking = get_voltages(sim, spiking_l23pc_gids, t_start, t_end)
-    _, v_subtreshold = get_voltages(sim, non_spiking_l23pc_gids, t_start, t_end)
-
+    _, _, v_spiking = get_voltages(sim, spiking_l23pc_gids, t_start, t_end, True)
+    non_spiking_l23pc_gids, t, v_subtreshold = get_voltages(sim, non_spiking_l23pc_gids, t_start, t_end, False)
     fig_name = os.path.join(FIGS_DIR, project_name, "L23PC_voltages.png")
-    plot_voltages(_clean_voltages(v_spiking, True), _clean_voltages(v_subtreshold, False), l23pc_rate,
-                  t_start, t_end, fig_name)
+    plot_all_voltages(v_spiking, v_subtreshold, l23pc_rate, t_start, t_end, fig_name)
+    # calculate EPSP amplitudes for the non-spiking group and plots the voltage trace corresponding to the 10 highest
+    v_subtreshold = v_subtreshold[:5000, :]  # sample because it would run forever
+    epsps = get_epsp_amplitudes(t, v_subtreshold, [2010, 2050])
+    fig_name = os.path.join(FIGS_DIR, project_name, "L23PC_subth_epsps.png")
+    plot_epsps_amplitudes(epsps, fig_name)
+    plot_gids, plot_vs = get_n_highest_epsp_voltages(non_spiking_l23pc_gids, v_subtreshold, epsps)
+    fig_name = os.path.join(FIGS_DIR, project_name, "most_active_subth_L23PC_vs.png")
+    plot_voltages(plot_gids, t, plot_vs, fig_name)
 
 
 if __name__ == "__main__":
---
+++ varani2021/plots.py
@@ -0,0 +1,88 @@
+"""
+SSCx analysis plots (related to the analysis of Varani et al. 2021)
+author: András Ecker, last update: 02.2022
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+
+sns.set(style="ticks", context="notebook")
+RED = "#e32b14"
+
+
+def plot_voltages(gids, t, voltages, fig_name):
+    """Plots voltage traces"""
+    fig = plt.figure(figsize=(20, 9))
+    gs = gridspec.GridSpec(len(gids), 1)
+    for i in range(len(gids)):
+        ax = fig.add_subplot(gs[i])
+        ax.plot(t, voltages[i, :], "k-")
+        ax.set_xlim([t[0], t[-1]])
+        ax.set_ylabel("a%s Vm (mV)" % gids[i])
+    ax.set_xlabel("Time (ms)")
+    sns.despine()
+    fig.tight_layout()
+    fig.savefig(fig_name, bbox_inches="tight", dpi=100)
+    plt.close(fig)
+
+
+def plot_all_voltages(v_spiking, v_subtreshold, rate, t_start, t_end, fig_name):
+    """Plots voltage collages of spiking and non-spiking cells"""
+    v_subtreshold_mean = np.mean(v_subtreshold, axis=0)
+    t_v = np.linspace(t_start, t_end, len(v_subtreshold_mean))
+    t_rate = np.linspace(t_start, t_end, len(rate))
+    xticks = np.linspace(t_start, t_end, 6).astype(int)
+    non_spiking_pct = (v_subtreshold.shape[0] / (v_spiking.shape[0] + v_subtreshold.shape[0])) * 100
+
+    fig = plt.figure(figsize=(20, 9))
+    gs = gridspec.GridSpec(4, 2, height_ratios=[1, 4, 15, 1], width_ratios=[69, 1])
+    ax = fig.add_subplot(gs[0, 0])
+    ax.plot(t_rate, rate, color=RED)
+    ax.fill_between(t_rate, np.zeros_like(t_rate), rate, color=RED, alpha=0.1)
+    ax.set_xticks(xticks)
+    ax.set_xlim(t_start, t_end)
+    ax.set_ylabel("Rate (Hz)")
+    sns.despine(ax=ax)
+    ax2 = fig.add_subplot(gs[1, 0])
+    i2 = ax2.imshow(v_spiking, cmap="inferno", aspect="auto", origin="lower")
+    plt.colorbar(i2, cax=fig.add_subplot(gs[1, 1]))
+    ax2.set_ylabel("Spiking gids")
+    ax2.set_xticks(np.linspace(0, v_spiking.shape[1], 6).astype(int))
+    ax2.set_xticklabels(xticks)
+    ax3 = fig.add_subplot(gs[2, 0])
+    i3 = ax3.imshow(v_subtreshold, cmap="inferno", aspect="auto", origin="lower")
+    cbar = plt.colorbar(i3, cax=fig.add_subplot(gs[2, 1]))
+    cbar.set_label("Voltage (mV)")
+    ax3.set_ylabel("Non-spiking gids (%.1f%%)" % non_spiking_pct)
+    ax3.set_xticks(np.linspace(0, v_subtreshold.shape[1], 6).astype(int))
+    ax3.set_xticklabels(xticks)
+    ax4 = fig.add_subplot(gs[3, 0])
+    ax4.plot(t_v, v_subtreshold_mean, "k-")
+    ax4.set_xticks(xticks)
+    ax4.set_xlim(t_start, t_end)
+    ax4.set_xlabel("Time (ms)")
+    ax4.set_ylabel("V (mV)")
+    ax4.set_yticks([np.floor(np.mean(v_subtreshold_mean))])
+    sns.despine(ax=ax4)
+    fig.align_ylabels()
+    gs.tight_layout(fig, h_pad=0.2, w_pad=0.2)
+    fig.savefig(fig_name, bbox_inches="tight", dpi=100)
+    plt.close(fig)
+    
+    
+def plot_epsps_amplitudes(epsps, fig_name):
+    """Plots EPSP amplitudes of non-spiking cells"""
+    range = [np.min(epsps), np.max(epsps)]
+    fig = plt.figure(figsize=(10, 6.5))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.hist(epsps, bins=30, range=range, color="gray")
+    ax.set_xlim([0, range[1]])
+    ax.set_xlabel("EPSP amplitude (mV)")
+    sns.despine(offset=5, trim=True)
+    fig.savefig(fig_name, bbox_inches="tight", dpi=100)
+    plt.close(fig)
+
