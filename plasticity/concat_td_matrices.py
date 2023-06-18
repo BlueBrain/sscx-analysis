fe7ecde9a599d4d7769a6995e55b6d81551ad3fc
Ecker András <andras.ecker@epfl.ch>
2023-06-18 20:19:08 +0200

    updated to the latest campaign

--- plasticity/concat_td_matrices.py
+++ plasticity/concat_td_matrices.py
@@ -5,14 +5,15 @@ author: András Ecker, last update: 06.2023
 
 import os
 import gc
+import warnings
 import h5py
 import pandas as pd
 from conntility.connectivity import TimeDependentMatrix
 from utils import load_sim_paths
 
 
-def load_td_matrix(sim_path, report_name):
-    h5f_name = os.path.join(os.path.split(sim_path)[0], "td_edges_%s.h5" % report_name)
+def load_td_matrix(sim_path, report_name, tag=""):
+    h5f_name = os.path.join(os.path.split(sim_path)[0], "td_edges_%s%s.h5" % (report_name, tag))
     return TimeDependentMatrix.from_h5(h5f_name)
 
 
@@ -47,22 +48,30 @@ def edges_to_h5(h5f_name, nodes, edge_idx, edges_at_0, edges, seeds, patterns):
 
 
 if __name__ == "__main__":
-    t = 120000  # ms
+    t = 122000  # ms
     report_name = "rho"
     project_name, seed = "3e3ef5bc-b474-408f-8a28-ea90ac446e24", 1  # all 10 patterns
-    conn_mat = load_td_matrix(load_sim_paths(project_name).loc[seed], report_name)
+    conn_mat = load_td_matrix(load_sim_paths(project_name).loc[seed], report_name, "_122000")
     nodes, edge_idx, edges_at_0 = extract_base_properties(conn_mat)
     edges = [edges_at_t(conn_mat, t, seed, "all")]
     del conn_mat
     project_name, seed = "19c8b9d7-5b06-435d-b93d-8277f0c858fe", 1  # none of the edges aka. spontaneous
-    conn_mat = load_td_matrix(load_sim_paths(project_name).loc[seed], report_name)
+    conn_mat = load_td_matrix(load_sim_paths(project_name).loc[seed], report_name, "_122000")
     edges.append(edges_at_t(conn_mat, t, seed, "none"))
     del conn_mat
     gc.collect()
 
-    project_name, seed = "d132adc8-2507-4a4e-8825-f9639d8d17e1", 1  # 3 patterns
+    project_name = "3b4f4df5-0f8d-4b5b-9fcf-5407bb9a7519"
     sim_paths = load_sim_paths(project_name)
-    for pattern, sim_path in sim_paths.items():  # this will become a MI with pattern *and* seed
+    level_names = sim_paths.index.names
+    for mi, sim_path in sim_paths.items():
+        for i, level_name in enumerate(level_names):
+            if level_name == "seed":
+                seed = mi[i]
+            elif "pattern" in level_name:
+                pattern = mi[i]
+            else:
+                warnings.warn("Unknown MultiIndex level name: %s" % level_name)
         conn_mat = load_td_matrix(sim_path, report_name)
         edges.append(edges_at_t(conn_mat, t, seed, pattern))
         del conn_mat
@@ -71,6 +80,6 @@ if __name__ == "__main__":
 
     h5f_name = os.path.join(os.path.split(os.path.split(sim_path)[0])[0], "grouped_td_edges_%s.h5" % report_name)
     # seeds and patterns will come from the MI
-    edges_to_h5(h5f_name, nodes, edge_idx, edges_at_0, edges, seeds=[1], patterns=["A", "B", "E"])
+    edges_to_h5(h5f_name, nodes, edge_idx, edges_at_0, edges, seeds=[], patterns=[])
 
 
