648f87cf54d3f6395022c7c4bc6efb67c4a8b5fb
Ecker András <andras.ecker@epfl.ch>
2023-07-12 11:53:31 +0200

    added analysis functions (developed for paper figures)

--- plasticity/concat_td_matrices.py
+++ plasticity/concat_td_matrices.py
@@ -1,12 +1,15 @@
 """
 Concatenates time dependent weighted connectomes from `conntility` from multiple sims. into one HDF5 file
-author: András Ecker, last update: 06.2023
+author: András Ecker, last update: 07.2023
 """
 
 import os
 import gc
 import warnings
 import h5py
+import numpy as np
+from scipy.spatial.distance import squareform
+from scipy.stats import wasserstein_distance
 import pandas as pd
 from conntility.connectivity import TimeDependentMatrix
 from utils import load_sim_paths
@@ -47,6 +50,112 @@ def edges_to_h5(h5f_name, nodes, edge_idx, edges_at_0, edges, seeds, patterns):
         grp.attrs["patterns"] = patterns
 
 
+def _get_edge_dists(edges, distance_metric, n_bins=1000):
+    """Gets Euclidean/Hamming/EM distance of last time steps of aggregated edges
+    Using `pdist` would probably parallelize it, but this way we have better control of e.g. how Hamming is computed..."""
+    assert distance_metric in ["Euclidean", "Hamming", "EMD"], "As the implementation doesn't use `pdist`" \
+                                                               "you'll need to implement it yourself"
+    columns = edges.columns.to_numpy()
+    row_idx, col_idx = np.triu_indices(len(columns), k=1)
+    dists = np.zeros_like(row_idx, dtype=np.float32)
+    if distance_metric == "EMD":
+        bin_edges = np.linspace(edges.min().min(), edges.max().max(), n_bins + 1)
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    for i, (row_id, col_id) in enumerate(zip(tqdm(row_idx, desc="Calculating distances"), col_idx)):
+        a, b = edges[columns[row_id]].to_numpy(), edges[columns[col_id]].to_numpy()
+        if distance_metric == "Euclidean":
+            dists[i] = np.linalg.norm(a - b)
+        elif distance_metric == "Hamming":
+            dists[i] = len(np.where(np.abs(a - b) > np.finfo(np.float32).eps)[0])  # don't compare against 0 ...
+        elif distance_metric == "EMD":
+            a_hist, _ = np.histogram(a, bin_edges)
+            b_hist, _ = np.histogram(b, bin_edges)
+            dists[i] = wasserstein_distance(bin_centers, bin_centers, a_hist, b_hist)
+    return dists, columns
+
+
+def get_all_edge_dists(edges, npzf_name=None):
+    """Gets all 3 distances for all edges"""
+    if npzf_name is None or (npzf_name is not None and not os.path.isfile(npzf_name)):
+        emds, columns = _get_edge_dists(edges, "EMD")
+        hamming_dists, _ = _get_edge_dists(edges, "Hamming")
+        euclidean_dists, _ = _get_edge_dists(edges, "Euclidean")
+        if npzf_name is not None:
+            np.savez(npzf_name, emds=emds, hamming_dists=hamming_dists, euclidean_dists=euclidean_dists, columns=columns)
+    else:
+        tmp = np.load(npzf_name, allow_pickle=True)
+        columns = tmp["columns"]
+        emds, hamming_dists, euclidean_dists = tmp["emds"], tmp["hamming_dists"], tmp["euclidean_dists"]
+    return emds, hamming_dists, euclidean_dists, columns
+
+
+def _get_ticks(columns):
+    """Get ticks and ticklabels from MultiIndex columns (converted to a numpy array)
+    The function assumes that repetitions of the same pattern are ordered and pattern name is the first index..."""
+    pattern_names, idx = np.unique([col[0] for col in columns], return_inverse=True)
+    ticks, ticklabels = [], []
+    for pattern_name, pattern_id in zip(pattern_names, np.unique(idx)):
+        ticklabels.append(pattern_name)
+        tmp = np.where(idx == pattern_id)[0]
+        tick = tmp[len(tmp) // 2] if len(tmp) % 2 != 0 else (tmp[int(len(tmp) / 2 - 1)] + tmp[int((len(tmp) / 2))]) / 2
+        ticks.append(tick)
+    return ticks, ticklabels
+
+
+def _pair_dists(pattern_dists, pattern_names, dists, columns):
+    """Repeats `pattern_dists` (in the same order `dists` are) and indexes out the ones with non-zero distance
+    (As above it assumes that pattern name is the first in the index)"""
+    assert len(dists) > len(pattern_dists)
+    pattern_dists_mat = squareform(pattern_dists)
+    pattern_names_dict = {pattern_name: id for id, pattern_name in enumerate(pattern_names)}
+    pattern_dists_rep = np.zeros_like(dists)
+    row_idx, col_idx = np.triu_indices(len(columns), k=1)
+    for i, (row_id, col_id) in enumerate(zip(row_idx, col_idx)):
+        pattern_dists_rep[i] = pattern_dists_mat[pattern_names_dict[columns[row_id][0]],
+                                                 pattern_names_dict[columns[col_id][0]]]
+    idx = np.where(pattern_dists_rep != 0.)[0]
+    return pattern_dists_rep[idx], [dists[idx]]
+
+
+def get_chaning_edges(edges_0, edges):
+    """For each pattern, get number of times an edge is changing (across seeds)"""
+    edge_idx, changing_edges = np.zeros_like(edges.index.to_numpy()), {}
+    for column in edges.columns.to_numpy():
+        pattern_name, seed = column[0], column[1]  # assemes fix order
+        diffs = edges[column] - edges_0
+        idx = diffs[diffs != 0.].index.to_numpy()
+        if pattern_name not in changing_edges:
+            changing_edges[pattern_name] = edge_idx.copy()
+        changing_edges[pattern_name][idx] += 1
+    return pd.DataFrame.from_dict(changing_edges)
+
+
+def _only_max_changes(changing_edges):
+    """Keeps only edges that change in all (actually max per pattern...) seeds"""
+    changing_edges_bool = changing_edges.copy()
+    for pattern_name, max_count in changing_edges.max().items():
+        changing_edges_bool.loc[(0 < changing_edges[pattern_name]) & (changing_edges[pattern_name] < max_count),
+        pattern_name] = 0
+    return changing_edges_bool
+
+
+def get_changing_nodes(nrn, edge_idx, changing_edges, only_max=True):
+    """Gets gids that have edges changing (separated for pre and post population)
+    As this usually results in almost the full set of gids some thresholding could be added..."""
+    if only_max:  # keep only edges that change in all seeds (per pattern)
+        changing_edges_bool = _only_max_changes(changing_edges)
+    else:  # get rid of counts and keep everything that's changing
+        changing_edges_bool = changing_edges.copy()
+        changing_edges_bool[changing_edges >= 1] = 1
+
+    changing_gids = {}
+    for pattern_name in changing_edges_bool.columns.to_numpy():
+        df = edge_idx.loc[changing_edges_bool.loc[changing_edges_bool[pattern_name] == 1, pattern_name].index]
+        changing_gids[pattern_name] = {"pre": nrn.loc[df["row"].unique(), "gid"].to_numpy(),
+                                       "post": np.sort(nrn.loc[df["col"].unique(), "gid"].to_numpy())}
+    return changing_gids
+
+
 if __name__ == "__main__":
     t = 122000  # ms
     report_name = "rho"
@@ -83,5 +192,7 @@ if __name__ == "__main__":
     edges_to_h5(h5f_name, nodes, edge_idx, edges_at_0, edges,
                 seeds=sim_paths.index.get_level_values("seed").unique().to_numpy().tolist(),
                 patterns=sim_paths.index.get_level_values(level_name).unique().to_numpy().tolist())
+
+    # TODO call the analysis functions...
 
 
